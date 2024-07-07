0b056ebe246ddbdbf0631f45610c24f98c530e09
GobuuriNaresh <nareshgobburi77@gmail.com>
2024-07-07 23:41:21 +0530

    correction

--- server/util/database.js
+++ server/util/database.js
@@ -7,4 +7,12 @@ const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,proc
   host: process.env.DB_HOST
 });
 
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 module.exports = sequelize;
